c0fc1caff2ea665af73ecee8a716f09c88b31e5a
avinash <akaushik509@gmail.com>
2023-01-20 22:24:20 +0530

    product update

--- nutribest.com/Components/ProductPageComp/ProductSlider.jsx
+++ nutribest.com/Components/ProductPageComp/ProductSlider.jsx
@@ -12,7 +12,7 @@ export default function ProductSlider({data}) {
     speed: 2000,
     slidesToShow: 3,
     slidesToScroll: 3,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 3000,
     responsive: [
       {
