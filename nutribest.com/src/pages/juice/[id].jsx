75b0c182674a5a1bc7306d90a6536397aa83b415
avinash <akaushik509@gmail.com>
2023-01-21 15:31:58 +0530

    day5

--- nutribest.com/src/pages/juice/[id].jsx
+++ nutribest.com/src/pages/juice/[id].jsx
@@ -1,15 +1,14 @@
 import React, { useState} from 'react';
-
+import style from "./avijuice.module.css"
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/router";
-
+import { addProduct } from '@/redux/SingleProduct/action';
 import { Flex, Box, Heading, Button, Text } from '@chakra-ui/react';
 
 import Footer2 from 'Components/Footer/Footer2';
 
 
 const workflow1 = [
-  
   {
     title: "Product Details",
     content:
@@ -37,6 +36,20 @@ const workflow1 = [
   },
 ];
 
+const handleAddToCart = (todo) => {
+  return fetch("http://localhost:8080/Cart", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(todo)
+  }).then((res) =>{ 
+    res.json()
+    alert("Added")
+  });
+};
+
+
 const Page = ({product}) => {
 
   const [dataWorkflow, setDataWorkFlow] = useState(workflow1[0]);
@@ -47,39 +60,38 @@ const Page = ({product}) => {
          }
         })
        }
-    const dispatch = useDispatch();
-    const router = useRouter();
+
     console.log(product);
     const {active,id,product_title,product_price,product_star_rating,climate_pledge_friendly,product_num_ratings,product_photo,product_minimum_offer_price,is_best_seller,is_prime} = product[0];
   return (
     <div>
-        <Box width={"85%"} margin="auto" marginTop={"20px"}>
-            <Flex gap={"50px"} border="1px solid red" width={"90%"} margin="auto">
-                <Box border="1px solid black" w="40%" h={"700px"}>
-                  <Box w={"95%"} h="80%" border="1px solid red" overflow={"hidden"}display="inline-flex" >
-                    <center><img src={product_photo} style={{objectFit:"cover"}} /></center>
+        <Box width={"85%"} margin="auto" marginTop={"150px"} id={style.juice1} key={id}>
+            <Flex gap={"40px"} width={"90%"} margin="auto" id={style.juice2}>
+                <Box w="40%" h={"700px"} id={style.juice21}>
+                  <Box w={"100%"} h="70%" id={style.juice211}  overflow={"hidden"} display="inline-flex"  >
+                    <center><img src={product_photo} width="100%" height={"100%"} style={{objectFit:"cover"}} /></center>
                   </Box>
-                  <Box border={"1px solid blue"} width="40%" margin={"auto"} marginTop="30px" gap={"20px"}>
-                    <Heading size={"md"}>Product Benefits</Heading>
-                    <Heading size={"xs"}><span>✅</span>Comprise of MG</Heading>
-                    <Heading size={"xs"}><span>✅</span>{climate_pledge_friendly?"Yes":"No"}</Heading>
+                  <Box id={style.juice212} width="40%" margin={"auto"} marginTop="30px" gap={"20px"} >
+                    <Heading id={style.head} size={"md"}>Product Benefits</Heading>
+                    <Heading id={style.head} size={"xs"}><span>✅</span>Comprise of MG</Heading>
+                    <Heading id={style.head} size={"xs"}><span>✅</span>{climate_pledge_friendly?"Yes":"No"}</Heading>
                   </Box>
                 </Box>
-                <Box border={"1px solid yellow"} w="60%">
-                    <Heading size={"lg"} textAlign="justify" marginBottom="20px" >{product_title}</Heading>
-                    <Heading size={"md"} marginBottom="20px"> Star Rating ⭐⭐⭐⭐{product_star_rating}</Heading>
-                    <Heading size={"md"} marginBottom="20px"> Min. Price: Rs.{Number(product_minimum_offer_price).toFixed(2)*80}</Heading>
-                    <Heading size={"md"} marginBottom="20px"> Price: Rs.{Number(product_price).toFixed(2)*80}</Heading>
-                    <Button marginBottom="20px"><img src="https://static1.hkrtcdn.com/hknext/static/media/pdp/Buy.svg" onClick={() => {dispatch(addProduct({product_title,product_price,id,product_photo}))}} /> Add To Cart</Button>
-                    <Flex gap={"30px"}>
+                <Box w="60%" id={style.juice22}>
+                    <Heading size={"lg"} id={style.head} textAlign="justify" marginBottom="20px" >{product_title}</Heading>
+                    <Heading size={"md"} id={style.head} marginBottom="20px"> Star Rating ⭐⭐⭐⭐{product_star_rating}</Heading>
+                    <Heading size={"md"} id={style.head} marginBottom="20px"> Min. Price: $ {(product_minimum_offer_price)}</Heading>
+                    <Heading size={"md"} id={style.head} marginBottom="20px"> Price: $ {(product_price)}</Heading>
+                    <center><Button marginBottom="20px"  onClick={()=>handleAddToCart(product[0])}><img src="https://static1.hkrtcdn.com/hknext/static/media/pdp/Buy.svg" /> Add To Cart</Button></center> 
+                    {/* <Flex gap={"30px"}>
                       <img src="https://static1.hkrtcdn.com/hknext/static/media/pdp/weight.svg" alt="err" />
-                      <Heading size={"md"} marginTop="7px"> Weight</Heading>
-                      <Button>KG</Button>
-                    </Flex>
+                      <Heading id={style.head} size={"md"} marginTop="7px"> Weight</Heading>
+                      <Button >KG</Button>
+                    </Flex> */}
 
-                    <Box border={"1px solid red"} w="500px" margin={"auto"} marginTop="30px">
-                      <Flex gap="10px" justifyContent={"center"} >
-                        <Box border={"1px solid gray"} w="50%"  padding={"10px"} display={"grid"} alignItems={"center"} justifyContent="center">
+                    <Box w="500px" margin={"auto"} marginTop="30px" id={style.juice23}>
+                      <Flex gap="5px" justifyContent={"center"} id={style.juice231} >
+                        <Box w="50%" border={"1px solid gray"}  padding={"10px"} display={"grid"} alignItems={"center"} justifyContent="center">
                           <img style={{ margin:"auto", marginBottom:"15px"}} src="https://static1.hkrtcdn.com/hknext/static/media/pdp/fassai_logo.svg" alt="Error" />
                           <p>Lic. No. 10015064000576</p>
                         </Box>
@@ -96,11 +108,11 @@ const Page = ({product}) => {
             </Flex>
 
             <Box w="90%" margin={"auto"} marginTop="30px">
-              <Flex id="box9-0">
+              <Flex id={style.box90}>
                   
                   {workflow1.map((el) => {
                       return (
-                          <Button id="btnwork"
+                          <Button id={style.btnwork}
                           onClick={()=>{handleChangeWorkflow(el.title)}}
                           borderBottom={
                               dataWorkflow.title === el.title ? "4px solid #5034ff" : ""
@@ -115,14 +127,14 @@ const Page = ({product}) => {
                           bg="white"
                           >
                           
-                          <Text>{el.title}</Text>
+                          <Text id={style.head1}>{el.title}</Text>
                           </Button>
                       );
                       })}
               </Flex>
-              <Flex id="box9-1" border={"1px solid red"} marginTop="30px">
-                  <Box id="box9-1b">
-                      <Box id="box9-1c">    
+              <Flex marginTop="30px">
+                  <Box >
+                      <Box >    
                           <Text color="#6d6dff" fontSize="30px" >{dataWorkflow.title}</Text>
                       </Box>
                       <Text color="grey" marginTop="20px" fontSize="20px">{dataWorkflow.content}</Text>
@@ -158,6 +170,20 @@ const Page = ({product}) => {
         product:data,
       },
     }
-  }
+  } 
+
+ /*  export async function getServerSideProps(context) {
+    console.log("context", context);
+    const {
+      params:{id},
+    } = context;
+    let res = await fetch(`http://localhost:8080/Healthy_Juice/?id=${id}`);
+    let data = await res.json();
+    return {
+      props:{
+        product:data,
+      },
+    }
+  } */
 
 export default Page 
